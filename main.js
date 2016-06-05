a7fedade5d17a77d2c9d7ae5a0b918c5debe72dc
Phalanstere <burckhardt@ludicmedia.de>
2016-06-05 17:29:39 +0200

    versin 0.46

--- main.js
+++ main.js
@@ -10,7 +10,14 @@ let win;
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({width: 1400, height: 900});
+  win = new BrowserWindow({width: 1400, 
+                           height: 900, 
+                           icon: __dirname + '/feather.ico'
+                           });
+
+
+
+
 
   // and load the index.html of the app.
   win.loadURL(`file://${__dirname}/index.html`);
